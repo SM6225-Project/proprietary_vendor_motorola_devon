410b0e273548847d07b8a56704879aa2b2db405b
josexda <josegustavo6@protonmail.com>
2024-04-02 07:31:04 -0300

    Revert "devon: Import firmware from T2SN33.73-22-3"
    This reverts commit 79e9cb70f318f21a5cbfb3072513d0e1ab49bd90.

--- Android.mk
+++ Android.mk
@@ -6,22 +6,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),devon)
 
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,bd4fda702cd2325bab12fccc797a1c34ec72046b)
-$(call add-radio-file-sha1-checked,radio/modem.img,c60a0869d0f2f5f1ff7c25bdf63b6779802590d7)
-$(call add-radio-file-sha1-checked,radio/abl.img,748d3744fab5476572aa106a1614b07ccc85da57)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,86228648a58020db1ad41a794109b59c89f0a9cd)
-$(call add-radio-file-sha1-checked,radio/dsp.img,e696f95a5b672f1b1355745ca05840b098cb6775)
-$(call add-radio-file-sha1-checked,radio/fsg.img,92106a524eaf0111539a23a8f39379a0fd35a477)
-$(call add-radio-file-sha1-checked,radio/hyp.img,92b7de5b7b04d2cc3ad9c74866bbe935316de787)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,e0a39746bdd1748eabff783a33ed300165b01f38)
-$(call add-radio-file-sha1-checked,radio/logo.img,5d80b190b4108757ccce4df4787a1ff6635758c7)
-$(call add-radio-file-sha1-checked,radio/prov.img,590cdc86baf03dba39f8b10998eb7650814b69b3)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,2cc33e203bca5ebad26b57703627037d8bbde3ac)
-$(call add-radio-file-sha1-checked,radio/rpm.img,bed4773f85ddc6e7f3af402987e9f6dacf39e8e5)
-$(call add-radio-file-sha1-checked,radio/storsec.img,d4e95e0e4c32c4574a920e7d9fd79bbb51f5e2a9)
-$(call add-radio-file-sha1-checked,radio/tz.img,c692bc57c6a63291ddf8d4a28a35e8269e4c1e4e)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,615150bfa7b9abe36ade1443435cdfd921a7ade1)
-$(call add-radio-file-sha1-checked,radio/xbl.img,b243bfc1b8e0d80de9da24eb49bea52de9580df3)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,a31214b82e4ec34d4d0ec793f9dcbf4764269ba0)
-
 endif
